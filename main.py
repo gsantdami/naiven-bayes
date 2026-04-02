584e68791ed598832f801f3fd93133f88bcfd481
gsantdami <topchernobyl@gmail.com>
2026-04-02 12:49:22 -0300

    adiciona calculo de probabilidade de um usuário gostar.

--- main.py
+++ main.py
@@ -35,12 +35,32 @@ def get_likelihood(features, prior, smoothing=0):
         likelihood[label] = likelihood[label] / (total + 2 * smoothing)
     return likelihood
 
+def get_posterior(X, prior, likelihood):
+    posteriors = []
+    for x in X:
+        posterior = prior.copy()
+        for label, likelihood_label in likelihood.items():
+            for index, bool_value in enumerate(x):
+                posterior[label] *= likelihood_label[index] if bool_value else (1 - likelihood_label[index])
+        
+        sum_posterior = sum(posterior.values())
+        for label in posterior: 
+            if posterior[label] == float('inf'):
+                posterior[label] = 1.0
+
+            else:
+                posterior[label] /= sum_posterior
+        posteriors.append((posterior.copy()))
+    return posteriors
+
+
 
 label_indexes = get_labels_index(Y_train)
 prior = get_prior(label_indexes)
-print(f"Prior: {prior}")
 
 smoothing = 1
 likelihood = get_likelihood(X_train, label_indexes, smoothing)
 
-print(f"Likelihood: {likelihood}")
+
+posterior = get_posterior(X_test, prior, likelihood)
+print(f"POSTERIOR: {posterior}")
