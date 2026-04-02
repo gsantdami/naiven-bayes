5d2ebff03ddace33a0e3814b5357ef1880a27147
gsantdami <topchernobyl@gmail.com>
2026-04-02 12:07:55 -0300

    adiciona cálculo de verossimilhança

--- main.py
+++ main.py
@@ -27,6 +27,20 @@ def get_prior(label_indexes):
         prior[label] /= total
     return prior
 
+def get_likelihood(features, prior, smoothing=0):
+    likelihood = {}
+    for label, index in label_indexes.items():
+        likelihood[label] = features[index, :].sum(axis=0) + smoothing
+        total = len(index)
+        likelihood[label] = likelihood[label] / (total + 2 * smoothing)
+    return likelihood
+
+
 label_indexes = get_labels_index(Y_train)
 prior = get_prior(label_indexes)
 print(f"Prior: {prior}")
+
+smoothing = 1
+likelihood = get_likelihood(X_train, label_indexes, smoothing)
+
+print(f"Likelihood: {likelihood}")
